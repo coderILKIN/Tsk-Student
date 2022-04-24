51ea75339496094c64b1916fed099e39f9f2c7b7
Ilkin Yusubov <tu758403c@code.edu.az>
2022-04-24 07:51:29 +0400

    task add

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = Console.ReadLine();
+
+            Student student = new Student("Ilkin Yusubov",subject);
+
+             subject = Console.ReadLine();
+
+            Student student1 = new Student("Tahir Memmedeov",subject);
+
+            Console.WriteLine(student.GroupName() + student.Count);
+            Console.WriteLine(student1.GroupName() + student1.Count);
+
+            student.getinfo();
+            student1.getinfo();
+        }
+    }
+}
---
+++ ConsoleApp21/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp21
+{
+    class Student
+    {
+        private static int id;
+        public int Id { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Subject { get; set; }
+
+        private static int count = 1000;
+        public int Count { get; set; }
+
+
+
+
+        public Student(string fullname,string subject)
+        {
+            Id = ++id;
+            Count = ++count;
+            Fullname = fullname;
+            Subject = subject;
+        }
+
+
+        public string GroupName()
+        {
+            return char.ToUpper(Subject[0]) + char.ToUpper(Subject[1]).ToString();
+        }
+
+        public void getinfo()
+        {
+            Console.WriteLine($"Id: {Id},Fullname: {Fullname},Subject: {Subject}, Count: {Count}");
+        }
+
+
+    }
+}
